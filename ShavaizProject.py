3364afced0f4115517eed3ad8767e93eaf5befb2
shahwahababdul <shahwahababdul761@gmail.com>
2025-03-10 22:13:32 -0700

    Update ShavaizProject.py

--- ShavaizProject.py
+++ ShavaizProject.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import matplotlib.pyplot as plt
 from io import BytesIO
 from matplotlib.backends.backend_pdf import PdfPages
